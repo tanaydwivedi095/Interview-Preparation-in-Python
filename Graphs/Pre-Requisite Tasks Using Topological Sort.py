0fba1c959661a7b649aa84f9e99f76db218995c8
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2023-05-05 15:44:07 +0530

    Create Pre-Requisite Tasks Using Topological Sort.py

---
+++ Graphs/Pre-Requisite Tasks Using Topological Sort.py
@@ -0,0 +1,40 @@
+def bfs(queue):
+    while queue:
+        currNode = queue.pop(0)
+        result.append(currNode)
+        for neighbour in adjList[currNode]:
+            inDegree[neighbour] -= 1
+            if inDegree[neighbour] == 0:
+                queue.append(neighbour)
+        
+def inDegreeCreator(n,adjList):
+    indegree = [0 for i in range(0,n)]
+    for neighbours in adjList.values():
+        for node in neighbours:
+            indegree[node] += 1
+    return indegree
+        
+def adjListCreator(n, edges):
+    adjList = {i:[] for i in range(0,n)}
+    for edge in edges:
+        start, end = edge
+        adjList[start].append(end)
+    return adjList
+    
+if __name__ == "__main__":
+    n = 5
+    result = []
+    edges = [[0,1],[1,2],[4,3],[2,4]]
+    adjList = adjListCreator(n, edges)
+    inDegree = inDegreeCreator(n, adjList)
+    visited = [0 for i in range(0,n)]
+    queue = []
+    for i in range(0,n):
+        if inDegree[i] == 0:
+            queue.append(i)
+    bfs(queue)
+    flag = len(result)==n
+    if flag == True:
+        print(result)
+    else:
+        print(-1, result)
