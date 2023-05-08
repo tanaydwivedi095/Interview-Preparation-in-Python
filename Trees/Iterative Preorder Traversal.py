c9dd173b9b6baf6938c3563e073b637e2ecc5b3e
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2023-05-08 05:50:00 +0530

    Create Iterative Preorder Traversal.py

---
+++ Trees/Iterative Preorder Traversal.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0):
+        self.left = None
+        self.val = val
+        self.right = None
+        
+def inorder(root):
+    stack = [root]
+    res = []
+    while stack:
+        currNode = stack.pop(-1)
+        if currNode!=None:
+            stack.append(currNode.right)
+            stack.append(currNode.left)
+            res.append(currNode.val)
+    return res
+        
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(2)
+    root.right = TreeNode(4)
+    root.left.left = TreeNode(1)
+    root.right.right = TreeNode(5)
+    print(inorder(root))
