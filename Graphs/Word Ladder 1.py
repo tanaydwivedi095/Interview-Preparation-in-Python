7ee6e7985e3762d6da4375843cf21d01763b6b8b
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2023-05-11 20:00:36 +0530

    Create Word Ladder 1.py

---
+++ Graphs/Word Ladder 1.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from collections import deque
+
+def wordLadderLength(startWord, targetWord, wordList):
+    if endWord not in wordList:
+        return 0
+    nei=defaultdict(list)
+    wordList.append(beginWord)
+    for word in wordList:
+        for j in range(len(word)):
+            pattern=word[:j]+"*"+word[j+1:]
+            nei[pattern].append(word)
+    visit=set([beginWord])
+    q=deque([beginWord])
+    count=1
+    while q:
+        for i in range(len(q)):
+            word=q.popleft()
+            if word==endWord:
+                return count
+            for j in range(len(word)):
+                pattern=word[:j]+"*"+word[j+1:]
+                for neiword in nei[pattern]:
+                    if neiword not in visit:
+                        visit.add(neiword)
+                        q.append(neiword)
+        count+=1
+    return 0
+
+if __name__ == "__main__":
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot","dog","dot","lot","log","cog"]
+    print(wordLadderLength(beginWord, endWord, wordList))
