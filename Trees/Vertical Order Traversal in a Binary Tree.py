311fba060fb1ed807dd17d9cf596d9841d7b6683
Tanay Dwivedi <84758723+tanaydwivedi095@users.noreply.github.com>
2023-05-13 22:53:33 +0530

    Update Vertical Order Traversal in a Binary Tree.py

--- Trees/Vertical Order Traversal in a Binary Tree.py
+++ Trees/Vertical Order Traversal in a Binary Tree.py
@@ -11,19 +11,30 @@ def verticalOrder(root):
     while queue:
         currNode = queue.pop(0)
         node, x, y = currNode
-        print(node.val, end=" ")
         if node.left != None:
             queue.append((node.left, x-1, y+1))
         if node.right != None:
             queue.append((node.right, x+1, y+1))
-        if y not in levelMap:
-            levelMap[y] = {x:node.val}
+        if x not in levelMap:
+            levelMap[x] = {y:[node.val]}
         else:
-            levelMap[y][x] = node.val
+            temp = levelMap[x]
+            if y not in temp:
+                levelMap[x][y] = [node.val]
+            else:
+                levelMap[x][y].append(node.val)
     for k,v in levelMap.items():
         lst = sorted(v.values())
         levelMap[k] = lst
-    return list(levelMap.values())
+    res = []
+    lst = list(levelMap.values())
+    for i in range(0,len(lst)):
+        temp = []
+        for j in range(0,len(lst[i])):
+            for k in range(0,len(lst[i][j])):
+                temp.append(lst[i][j][k])
+        res.append(temp)
+    return res
     
     
 def treeCreator():
@@ -31,6 +42,7 @@ def treeCreator():
     root.left = TreeNode(2)
     root.right = TreeNode(3)
     root.left.left = TreeNode(4)
+    root.left.right = TreeNode(10)
     root.right.left = TreeNode(9)
     root.right.right = TreeNode(10)
     root.left.left.right = TreeNode(5)
